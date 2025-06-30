aedb6a1105ea96a45254f80420092ca19a24fac6
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2025-06-29 21:40:47 -0400

    Changed the JS file name, and added the class and constructor and field variables for it

---
+++ SimonGame.js
@@ -0,0 +1,22 @@
+
+class SimonGame {
+    //region Field Variables
+    #colorList;
+    #colorSequence;
+    #crtLevel;
+    #playerSequence;
+    //endregion
+
+    //region Constructor
+    constructor() {
+        // This is where you initialize all field variables
+        this.#colorList = ["purple", "orange", "pink", "silver"];
+        this.#colorSequence = [];
+        this.#crtLevel = 0;
+        this.#playerSequence = [];
+    }
+    //endregion
+
+
+
+}
