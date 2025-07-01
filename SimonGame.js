d509c2b27d08534b5c12fbc987a588107c1ddba1
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2025-07-01 18:42:40 -0400

    This file was along with the previous commit

--- SimonGame.js
+++ SimonGame.js
@@ -39,6 +39,78 @@ class SimonGame {
 
     //region Methods
     startGame() {
+        document.getElementById("startButton").style.display = "none";
+        document.getElementById("title").style.display = "none";
+        document.getElementById("buttonBox").style.display = "grid";
+
+
+        /*
+        this.addColorToSequence()
+        // play the color sequence with the matching sound
+        this.#colorSequence.forEach((color) => {
+            switch(color) {
+                case "purple":
+                    this.playPurpleSound();
+                    break;
+                case "orange":
+                    this.playOrangeSound();
+                    break;
+                case "pink":
+                    this.playPinkSound();
+                    break;
+                case "silver":
+                    this.playSilverSound();
+                    break;
+            }
+        })
+         */
+
+
+        // let the player try to match the sequence
+
+
+        // figure out if the player's sequence matches with simon's color sequence
+
+
+
+
+    }
+
+    addColorToSequence() {
+        // Retrieve a random number from 0 to 3
+        let randomColor = this.randomNumber;
+
+        // Add the color to the sequence based on the number from the colorList index
+        this.#colorSequence.push(this.#colorList[randomColor]);
+    }
+
+    /*
+    playPinkSound() {
+        const pink = new Audio("sound/orange.mp3");
+        pink.volume = 0.5;
+        pink.play();
+    }
+
+    playOrangeSound() {
+        const orange = new Audio("sound/orange.mp3");
+        orange.volume = 0.5;
+        orange.play();
+    }
+
+    playPurpleSound() {
+        const purple = new Audio("sound/purple.mp3");
+        purple.volume = 0.5;
+        purple.play();
+    }
+
+    playSilverSound() {
+        const silver = new Audio("sound/silver.mp3");
+        silver.volume = 0.5;
+        silver.play();
+    }
+    */
+
+    showSequence() {
 
     }
 
