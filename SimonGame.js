4a48760508b026734f6957b1ef85875329f7e16f
Faris Zulfequar <zulfequf@shernet.sheridancollege.ca>
2025-08-23 15:57:57 -0400

    Convert the color and sound mapping dictionaries to static class properties, and add a getter-setter pair for highestLevel

--- SimonGame.js
+++ SimonGame.js
@@ -1,10 +1,22 @@
 
 class SimonGame {
     //region Field Variables
-    #colorList;
+    static colorList = ["purple", "orange", "pink", "silver"];
+    static colorIDToKey = { "purpleBtn" : "purple",
+        "orangeBtn" : "orange",
+        "pinkBtn" : "pink",
+        "silverBtn" : "silver",
+        "sound-example1" : "purple",
+        "sound-example2" : "orange",
+        "sound-example3" : "pink",
+        "sound-example4" : "silver",
+    }
+    static colorMapSound = { "purple" : new Audio("sound/sound1.mp3"),
+        "orange" : new Audio("sound/sound2.mp3"),
+        "pink" : new Audio("sound/sound3.mp3"),
+        "silver" : new Audio("sound/sound4.mp3")
+    }
     #colorSequence;
-    #colorIDToKey;
-    #colorMapSound;
     #crtLevel;
     #highestLevel;
     #playerSequence;
@@ -12,21 +24,6 @@ class SimonGame {
 
     //region Constructor
     constructor() {
-        this.#colorList = ["purple", "orange", "pink", "silver"];
-        this.#colorIDToKey = { "purpleBtn" : "purple",
-            "orangeBtn" : "orange",
-            "pinkBtn" : "pink",
-            "silverBtn" : "silver",
-            "sound-example1" : "purple",
-            "sound-example2" : "orange",
-            "sound-example3" : "pink",
-            "sound-example4" : "silver",
-        }
-        this.#colorMapSound = { "purple" : new Audio("sound/sound1.mp3"),
-            "orange" : new Audio("sound/sound2.mp3"),
-            "pink" : new Audio("sound/sound3.mp3"),
-            "silver" : new Audio("sound/sound4.mp3")
-        }
         this.#colorSequence = [];
         this.#crtLevel = 0;
         this.#highestLevel = 0;
@@ -35,23 +32,20 @@ class SimonGame {
     //endregion
 
     //region Setters & Getters
-    get ColorList() {
-        return this.#colorList;
-    }
     get ColorSequence() {
         return this.#colorSequence;
-    }
-    get ColorIDToKey() {
-        return this.#colorIDToKey;
-    }
-    get ColorMapSound() {
-        return this.#colorMapSound;
     }
     get CrtLevel() {
         return this.#crtLevel;
     }
     set CrtLevel(newLevel) {
         this.#crtLevel = newLevel;
+    }
+    get HighestLevel() {
+        return this.#highestLevel;
+    }
+    set HighestLevel(newLevel) {
+        this.#highestLevel = newLevel;
     }
     get PlayerSequence() {
         return this.#playerSequence;
@@ -90,15 +84,15 @@ class SimonGame {
         let randomColor = this.randomNumber;
 
         // Add the color to the sequence based on the number from the colorList index
-        this.#colorSequence.push(this.#colorList[randomColor]);
+        this.#colorSequence.push(SimonGame.colorList[randomColor]);
     }
 
     PlayColoredSound(colorBtnID) {
         // first retrieve the color ID from the button
-        const soundID = this.#colorIDToKey[colorBtnID];
+        const soundID = SimonGame.colorIDToKey[colorBtnID];
 
         // retrieve the correct audio from the color
-        const colorSound = this.#colorMapSound[soundID];
+        const colorSound = SimonGame.colorMapSound[soundID];
 
         // play the audio
         colorSound.volume = 0.5;
